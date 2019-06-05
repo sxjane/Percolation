0dd42b11fe0e0fd5d360fedc8017809b8a6a254a
Xiaojie Si <sixiaojie.jane@gmail.com>
2019-06-05 11:49:25 +0200

    try nextjs

--- server/app.js
+++ server/app.js
@@ -1,16 +1,44 @@
+// const express = require("express");
+// const app = express();
+// const port = 4000;
+// const bodyParser = require("body-parser");
+// var morgan = require("morgan");
+
+// app.use(bodyParser.urlencoded({
+//     extended:true
+// }));
+// app.use(bodyParser.json());
+// app.use(morgan("dev"));
+
+// //var {matrix, ifPercolation} = require("./runtests/runPercolation")(10);
+
+// app.get("/", (req, res) => res.send(matrix, ifPercolation));
+// app.listen(port, ()=> console.log(`the app listening on port ${port}`));
+const next = require("next");
 const express = require("express");
-const app = express();
-const port = 3000;
-const bodyParser = require("body-parser");
-var morgan = require("morgan");
 
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
-app.use(bodyParser.json());
-app.use(morgan("dev"));
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-var {matrix, ifPercolation} = require("./runtests/runPercolation")(3);
+app.prepare().then(() => {
+    const server = express();
 
-app.get("/", (req, res) => res.send(matrix, ifPercolation));
-app.listen(port, ()=> console.log(`the app listening on port ${port}`));
+    server.get("/", (req,res) => {
+        return handle(req, res);
+    });
+
+    server.post("/", (req,res) =>{
+        console.log(req);
+        res.send("hello world");
+    });
+  
+    server.listen(3000, err =>{
+        if(err) throw err;
+        console.log("Ready on http://localhost:3000");
+    });
+})
+    .catch(ex => {
+        console.error(ex.stack);
+        process.exit(1);
+    });
